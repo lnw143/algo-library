aef82e05a2880e1043ca5cc09f7d40d30e88ae3f
lnw143 <lnwbkb143@163.com>
2024-10-14 21:59:19 +0800

    feat: Fast IO

--- tester.cpp
+++ tester.cpp
@@ -1,5 +1,76 @@
 #include <cstdio>
 #include <cstring>
+#include <string>
+#include <cctype>
+namespace IO {
+	static const int BUF = 1<<20;
+	static char pbuf[BUF],*p=pbuf;
+	inline char gc() {
+		static char buf[BUF],*p1=buf,*p2=buf;
+		return p1==p2&&(p2=(p1=buf)+fread(buf,1,BUF,stdin),p1==p2)?-1:*p1++;
+	}
+	inline void pc(char c) {
+		*p++=c;
+		if(p-pbuf==BUF) fwrite(pbuf,1,BUF,stdout),p=pbuf;
+	}
+	inline void flush() {
+		fwrite(pbuf,1,p-pbuf,stdout);
+		p=pbuf;
+	}
+	template<typename T> inline void read(T &x) {
+		x=0;
+		char c=gc();
+		T f=1;
+		for(; c<'0'||c>'9'; c=gc()) if(c=='-') f=-1;
+		for(; c>='0'&&c<='9'; c=gc()) x=(x<<3)+(x<<1)+(c^48);
+		x*=f;
+	}
+	inline void read(char *p) {
+		char c=gc();
+		for(; !isgraph(c); c=gc());
+		for(; isgraph(c); c=gc()) *p++=c;
+		*p='\0';
+	}
+	inline void read(std::string& s) {
+		s.clear();
+		char c=gc();
+		for(; !isgraph(c); c=gc());
+		for(; isgraph(c); c=gc()) s+=c;
+	}
+	template<typename T,typename ...Args> void read(T &x,Args &...args) {
+		read(x);
+		read(args...);
+	}
+	template<typename T> inline void write(const T &y) {
+		T x=y;
+		if(x<0) {
+			pc('-');
+			x=-x;
+		}
+		static char stk[1<<8],*tp;
+		tp=stk;
+		do *tp++=(x%10)^48; while(x/=10);
+		while(tp!=stk) pc(*--tp);
+	}
+	inline void write(const char &c) { pc(c); }
+	inline void write(const std::string &s) {
+		for(auto i : s) pc(i);
+	}
+	inline void write(const char *p) {
+		for(; *p; ++p) pc(*p);
+	}
+	inline void write(char *p) {
+		write(static_cast<const char*>(p));
+	}
+	template<typename T,typename ...Args> void write(const T &x,const Args &...args) {
+		write(x);
+		write(args...);
+	}
+	struct TMP { ~TMP() { flush(); } } tmp;
+};
+using IO::read;
+using IO::write;
+using IO::flush;
 #define _DEFAULT_BASE HEX
 #include "lnw143/bigint.h"
 
@@ -14,9 +85,8 @@ int main() {
 	freopen(".in","r",stdin);
 	freopen(".out","w",stdout);
 	LL a,b;
-	scanf("%s%s",s,t);
-	a=LL(s,strlen(s),HEX);
-	b=LL(t,strlen(t),HEX);
-	printf("%s\n",(a*b).to_string(HEX).c_str());
+	a.read(IO::gc);
+	b.read(IO::gc);
+	(a*b).write(IO::pc);
 	return 0;
 }
